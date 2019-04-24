7b7f0506b507307c9104e9ef4b317a081d521897
A Hashim <ahashim983@yahoo.com>
2019-04-24 14:39:15 +0100

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,10 +6,10 @@ attr_accessor :name, :age
 
   @@all = []
 
-  def initialize(name,age,breed)
+  def initialize(name,breed,age)
     @name = name
-    @age = age
     @breed = breed
+    @age = age
     @@all << self
 end
 
